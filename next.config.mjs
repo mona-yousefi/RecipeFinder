189d40340f69d2907fa3e0c19f9d61dc74b235cf
Mona <m.yousefinejad78@gmail.com>
2025-01-11 18:47:24 +0330

    next config again

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/Recipe-Finder'
 };
-module.exports = {
-  basePath: '/Recipe-Finder',
-  trailingSlash: true, // To ensure correct routing for GitHub Pages
-}
 
 export default nextConfig;
