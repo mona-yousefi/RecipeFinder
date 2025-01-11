9d862ca8fc4ecae21dbd8da9d275b8a5ecdb6600
Mona <m.yousefinejad78@gmail.com>
2025-01-11 20:36:52 +0330

    getstaticprops added

--- pages/recipe/[id].tsx
+++ pages/recipe/[id].tsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable @next/next/no-img-element */
 
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from "next";
 import styles from "../../styles/recipeDetail.module.css"
 import { MdFavoriteBorder } from 'react-icons/md';
 
@@ -58,7 +58,7 @@ const RecipeDetail: React.FC<RecipeDetailProps> = ({ recipe }) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticProps: GetStaticProps = async (context) => {
   const { id } = context.params!;
   const apiKey = 'b15461cfa5bc469aa1c0ac2cd3cba422';
 
