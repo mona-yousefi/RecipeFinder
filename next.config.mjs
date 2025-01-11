466cc9812975f78e4918b484a7da99cf0deae366
Mona <m.yousefinejad78@gmail.com>
2025-01-11 19:49:03 +0330

    more config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/Recipe-Finder'
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/Recipe-Finder/' : '',
+  basePath: isProd ? '/Recipe-Finder' : '',
+  output: 'export'
 };
 
 export default nextConfig;
